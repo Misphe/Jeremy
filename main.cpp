83eded4325c1e60ce1003b908ea9741cc62254a5
Misphe <124389712+Misphe@users.noreply.github.com>
2023-02-04 17:52:33 +0100

    created enemies; made platforms wider; fixed not being able to move bug

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #define _USE_MATH_DEFINES
-#include <math.h>
+#include<math.h>
 #include <stdio.h>
 #include <string.h>
 #include <iostream>
@@ -466,7 +466,7 @@ int main(int argc, char **argv) {
 				ResetPlatforms();
 				DoJump(delta);
 				Fall();
-				//GameOver();
+				GameOver();
 			}
 			DrawSprites();
 			DisplayPoints(timer);
